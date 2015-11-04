fd4ee901534f97199559278caf7a99a0e8a31a2a
Mike Hannon(code) <mjhannon@gmail.com>
2015-11-04 10:21:36 -0800

    testing initial class

---
+++ underscore.py
@@ -0,0 +1,14 @@
+class underscore(object):
+    def map(self):
+        pass
+    def find(self):
+        pass
+    def reduce(self):
+        pass
+    def reject(self):
+        pass
+    def filter(self):
+        pass
+
+print "working"
+_ = underscore()
