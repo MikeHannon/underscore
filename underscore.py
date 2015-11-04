6d3a004039d257ec8d0f48d4df72e688e581c6b2
Mike Hannon(code) <mjhannon@gmail.com>
2015-11-04 10:38:57 -0800

    hello explained

--- underscore.py
+++ underscore.py
@@ -1,6 +1,13 @@
 class underscore(object):
-    def map(self):
-        pass
+    def map(self, myLambda): #callback
+        #_.map(lambda x: x + " world") if we invoke _.map as seen on the left
+        #1) myLambda becomes lambda x: x + " world"
+        print (myLambda("hello"))
+        #2) myLambda expects a variable to replace x
+        #3) we invoke myLamda passing it "hello"
+        #4) x becomes hello and myLambda at this point looks like: lambda "hello": "hello" + " world"
+        #5) closing of the ) finalizes the invocation of the function.  print ('hello world')
+
     def find(self):
         pass
     def reduce(self):
@@ -11,4 +18,10 @@ class underscore(object):
         pass
 
 print "working"
+# var myFunction = function(){do something};// javascript anonymous function
+# function myFunction(){}
+# def myFunction():
+#   do something
+# myFunction = lambda x: do something
+
 _ = underscore()
